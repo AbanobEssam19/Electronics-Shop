e38c2df31b708087fd1ac9222bb8d5949da83b76
Abanob Essam <abanobessam19@gmail.com>
2024-09-10 19:05:02 +0300

    adding {bootstrap, fontawesome, files structure}

---
+++ app/components/Nav/nav.js
@@ -0,0 +1,56 @@
+import styles from "./nav.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+import Link from "next/link";
+
+library.add(fab, fas, far)
+
+function Nav1() {
+    return (
+        <div className={`container ${styles.nav1}`}>
+            <Link href="/" className={styles.navHeader}>
+                <div className={styles.logoContainer}>
+                    <img src="../logo.jpg" />
+                </div>
+                <h1>Electronic Shop</h1>
+            </Link>
+            
+            <div className={styles.searchBarContainer} >
+                <input type="text" className={styles.searchBar} />
+                <button type="submit" className={styles.searchBtn}><FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /></button>
+            </div>
+            <input type="button" value="Login" className={styles.loginBtn} />
+        </div>
+    )
+}
+
+function Nav2() {
+    return (
+        <div className={`container-fluid ${styles.nav2}`}>
+            <div className={`container`}>
+                <ul>
+                    <li>Home</li>
+                    <li>Categories</li>
+                    <li>About Us</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default function Nav() {
+    return (
+        <>
+            <Nav1 />
+            <Nav2 />
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import Nav from "./components/Nav/nav";
+
 export default function Home() {
   return (
-    <div>
-      <h1>MAZEN</h1>;
+    <div className={styles.mainPage}>
+      <Nav />
     </div>
   );
 }
