dcab8fbdc89c11123be82fe2f6ed9a1ff2c766d8
Abanob Essam <abanobessam19@gmail.com>
2024-09-11 13:25:19 +0300

    all nav and header is now completed

--- app/components/Nav/nav.js
+++ app/components/Nav/nav.js
@@ -1,3 +1,4 @@
+'use client'
 import '../../globals.css';
 import styles from "./nav.module.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -59,17 +60,33 @@ function Nav2() {
     return (
         <div className={`container-fluid ${styles.nav2}`}>
             <div className={`container ${styles.contentContainer}`} >
-                <ul>
+                <ul className={styles.mainNav}>
                     <li><Link href="#">Home</Link></li>
-                    <li><Link href="/">Categories</Link></li>
+                    <li className={styles.categoriesLink}>
+                        <Link href="/" >Categories <FontAwesomeIcon icon="fa-solid fa-caret-down" /></Link>
+                        <div className={styles.categoriesDropDown} >
+                            <ul>
+                                <li><Link href="#">Home</Link></li>
+                                <li><Link href="/">Categories</Link></li>
+                                <li><Link href="/">3D Printing</Link></li>
+                                <li><Link href="/">About Us</Link></li>
+                                <li><Link href="#">Home</Link></li>
+                                <li><Link href="/">Categories</Link></li>
+                                <li><Link href="/">3D Printing</Link></li>
+                                <li><Link href="/">About Us</Link></li>
+                                <li><Link href="#">Home</Link></li>
+                                <li><Link href="/">Categories</Link></li>
+                            </ul>
+                        </div>
+                    </li>
                     <li><Link href="/">3D Printing</Link></li>
                     <li><Link href="/">About Us</Link></li>
                 </ul>
                 
-                <div className={styles.menuBtn} >
+                <button className={styles.menuBtn} type='button' data-bs-toggle="offcanvas" data-bs-target="#navSideBar" >
                     <FontAwesomeIcon className={styles.menuIcon} icon="fa-solid fa-bars" />
                     <p>Menu</p>
-                </div>
+                </button>
                 
                 <div className={styles.searchBarContainer} >
                     <input type="text" className={styles.searchBar} placeholder='Search for products...' />
@@ -81,6 +98,44 @@ function Nav2() {
                 <button className={styles.navBtns} >
                     <FontAwesomeIcon icon="fa-solid fa-cart-shopping" style={{width: "40px", height: "30px"}} />
                 </button>
+            </div>
+
+            <div className={`offcanvas offcanvas-start`} id="navSideBar" style={{width: "350px"}}>
+                <div className={`offcanvas-header ${styles.sideBarHeader}`}>
+                    <div className={styles.sideBarLogo}>
+                        <img src='../side-bar-logo.png' />
+                    </div>
+                    <button type="button" data-bs-dismiss="offcanvas"><FontAwesomeIcon icon="fa-solid fa-xmark" style={{width: "20px", height: "20px"}} /></button>
+                </div>
+                <div className={`offcanvas-body ${styles.sideBarBody}`}>
+                    <li className={styles.sideNavItem}>
+                        <Link href="#"><FontAwesomeIcon icon="fa-solid fa-house" /> Home</Link>
+                    </li>
+                    <li className={styles.sideNavItem}>
+                        <button className={styles.categoriesBtn} data-bs-toggle="collapse" data-bs-target="#categoriesCollapse"><FontAwesomeIcon icon="fa-solid fa-list" /> Categories</button>
+                    </li>
+                    <div id="categoriesCollapse" className={`collapse ${styles.categoriesSideNav}`}>
+                        
+                        <li className={styles.sideNavItem}><Link href="#">Home</Link></li>
+                        <li className={styles.sideNavItem}><Link href="/">Categories</Link></li>
+                        <li className={styles.sideNavItem}><Link href="/">3D Printing</Link></li>
+                        <li className={styles.sideNavItem}><Link href="/">About Us</Link></li>
+                        
+                    </div>
+                    <li className={styles.sideNavItem}>
+                        <Link href="/"><FontAwesomeIcon icon="fa-brands fa-unity" /> 3D Printing</Link>
+                    </li>
+                    <li className={styles.sideNavItem}>
+                        <Link href="/"><FontAwesomeIcon icon="fa-solid fa-address-card" /> About Us</Link>
+                    </li>
+                    <li className={styles.sideNavItem}>
+                        <Link href="/"><FontAwesomeIcon icon="fa-solid fa-heart" /> Wishlist</Link>
+                    </li>
+                    <li className={styles.sideNavItem}>
+                        <Link href="/"><FontAwesomeIcon icon="fa-solid fa-user" /> Sign in</Link>
+                    </li>
+
+                </div>
             </div>
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 
 import Nav from "./components/Nav/nav";
 
+import { useEffect } from "react";
+
 export default function Home() {
+
+  useEffect(() => {
+      if (typeof window !== 'undefined') {
+        import('bootstrap/dist/js/bootstrap.bundle.min.js');
+      }
+  }, []);
+  
   return (
     <div className={styles.mainPage}>
       <Nav />
