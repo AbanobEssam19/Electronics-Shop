910eb9e6d144df5e69ec43093e4fc61636ee4239
Ahmad Abdul Hamid  Younes <96975157+AYOUNS@users.noreply.github.com>
2024-09-15 15:03:21 +0300

    15-9 printing update

--- app/pages/Printing/page.js
+++ app/pages/Printing/page.js
@@ -2,6 +2,8 @@
 import styles from "./3d.module.css";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "@/app/components/Nav/nav";
+import Footer from "@/app/components/Footer/footer";
 
 const Printing = () => {
   const [name, setName] = useState("");
@@ -29,62 +31,66 @@ const Printing = () => {
   };
 
   return (
-    <div className={styles.form_container}>
-      <h1>3D Printing Service Reservation</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Your Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="grams">Grams of Parts:</label>
-          <input
-            type="number"
-            id="grams"
-            value={grams}
-            onChange={(e) => setGrams(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="color">Filament Color:</label>
-          <input
-            type="text"
-            id="color"
-            value={color}
-            onChange={(e) => setColor(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="quality">Quality of Filament Needed:</label>
-          <input
-            type="text"
-            id="quality"
-            value={quality}
-            onChange={(e) => setQuality(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="file">Part File (STL):</label>
-          <input
-            type="file"
-            id="file"
-            accept=".stl"
-            onChange={(e) => setFile(e.target.files[0])}
-            required
-          />
-        </div>
-        <button type="submit">Send Order</button>
-      </form>
-    </div>
+    <>
+      <Nav />
+      <div className={styles.form_container}>
+        <h1>3D Printing Service Reservation</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Your Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="grams">Grams of Parts:</label>
+            <input
+              type="number"
+              id="grams"
+              value={grams}
+              onChange={(e) => setGrams(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="color">Filament Color:</label>
+            <input
+              type="text"
+              id="color"
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="quality">Quality of Filament Needed:</label>
+            <input
+              type="text"
+              id="quality"
+              value={quality}
+              onChange={(e) => setQuality(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="file">Part File (STL):</label>
+            <input
+              type="file"
+              id="file"
+              accept=".stl"
+              onChange={(e) => setFile(e.target.files[0])}
+              required
+            />
+          </div>
+          <button type="submit">Send Order</button>
+        </form>
+      </div>
+      <Footer />
+    </>
   );
 };
 
