2c60b90acbec386a64a6559383791387f7bb8fb7
mazenehab1126 <mazenronaldo2@gmail.com>
2024-10-06 17:01:07 +0200

    Myaccount => Order Page

--- app/pages/myaccount/orders/page.js
+++ app/pages/myaccount/orders/page.js
@@ -9,11 +9,32 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 
 library.add(fab, fas, far);
 
+function OrderDetails () {
+    return (
+        <div className= {styles.OrderDetails}>
+            <p style = {{color: "red"}}>#12323</p>
+            <p style = {{color: "grey"}}>September 15,2024 </p>
+            <p style = {{color: "grey"}}>Cancelled</p>
+            <p style = {{color: "black"}}>5000 <span style = {{color: "grey"}}>for 3 items</span></p>
+        </div>
+    )
+}
+
+
 export default function Orders () {
     return(
-        <div className={`container`} style={{minHeight : "500px"}}>
+        <div className={`container ${styles.Orders}`} style={{minHeight : "500px"}}>
             <MyaccountSidebar num ={2}/>
-            
+            <div className = {styles.OrderIcons}>
+                <div className = {styles.OrderText}>
+                    <p>ORDER</p>
+                    <p>DATE</p>
+                    <p>STATUS</p>
+                    <p>TOTAL</p>
+                </div>
+                <OrderDetails/> 
+            </div>
+
         </div>
 
 
